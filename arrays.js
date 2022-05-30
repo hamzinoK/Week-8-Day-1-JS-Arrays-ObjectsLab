fb20b2da6e715cde4a10688518dfd810ce58631b
Hamza Khan <hamza.kh28@gmail.com>
2022-05-30 20:34:07 +0100

    first commit

---
+++ arrays.js
@@ -0,0 +1,107 @@
+/**
+ * In these tasks you will need to manipulate the array below in order to answer the questions. Some answers will require you to store the result in a variable, others will need you to inspect the whole array.
+ */
+
+stations = ["Dundee", "Edinburgh", "North Berwick", "Newcastle", "York", "Leeds", "Doncaster", "Peterborough"];
+
+/**
+ * Q1.
+ * The East Coast Main Line doesn't stop at Peterborough! Add London Kings Cross to the end of the array to complete the line.
+ */
+
+stations.push("Kings Cross");
+
+// console.log(stations);
+
+
+
+/**
+ * Q2.
+ * It doesn't go as far as Dundee either, so remove it from the start of the array.
+ */
+
+stations.shift();
+
+// console.log(stations);
+
+
+
+/**
+ * Q3.
+ * How many stations are on the line?
+ */
+
+stationCount = stations.length;
+
+// console.log("There are " + stationCount + " stations.");
+
+
+
+/**
+ * Q4.
+ * Which station is the third stop?
+ */
+
+thirdStation = stations[2];
+
+// console.log("The third station is " + thirdStation);
+
+
+
+/**
+ * Q5.
+ * Someone put the wrong Berwick on the timetable! Update "North Berwick" to "Berwick-upon-Tweed".
+ */
+
+stations[1] = "Berwick upon tweed";
+
+// console.log(stations);
+
+
+
+/**
+ * Q6.
+ * How did Leeds get on there? Better remove it.
+ */
+
+const index = stations.indexOf('Leeds');
+if (index > -1) {
+    stations.splice(index,1);
+}
+
+
+console.log(stations);
+
+
+
+/**
+ * Q7.
+ * We forgot about Darlington! Add it to the route between Newcastle and York.
+ */
+
+stations.splice(3, 0, 'Darlington');
+
+console.log(stations);
+
+
+
+/**
+ * Q8.
+ * Is Durham one of the stops?
+ */
+
+stoppingAtDurham = stations.includes('Durham');
+
+console.log(stoppingAtDurham);
+
+
+/**
+ * Q9.
+ * If we are stopping at Durham, print "Yay Durham!" to the terminal. If not print "Aww...".
+ */
+
+if (stoppingAtDurham = 'false') {
+    console.log("Aww")
+} else {
+    console.log("Yay Durham!");
+}
